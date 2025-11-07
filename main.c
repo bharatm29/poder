d1497d3fe82f778cc29279a3a12e5e344a599b1a
bharatm29 <bharatmhes@gmail.com>
2025-11-07 19:01:02 +0530

    using typedefed types

--- main.c
+++ main.c
@@ -38,8 +38,16 @@
  */
 
 #define LENGTH 4
-#define CHAR sizeof(u_char)
+#define CHAR sizeof(uint8_t)
 #define CRC 4
+
+enum ColorType {
+    COLOR_GRAYSCALE = 0,
+    COLOR_TRUE_RGB = 2,
+    COLOR_INDEXED = 3,
+    COLOR_GRAYSCALE_ALPHA = 4,
+    COLOR_TRUEALPHA_RGBA = 6
+};
 
 void panic(const char *message) {
     printf("%s\n", message);
@@ -64,24 +72,24 @@ uLong zinflate(unsigned char *in, unsigned char *out, uLong comprLen,
 
     err = inflateInit(&d_stream);
     if (err != Z_OK) {
-        panic("error with inflate init\n");
+        panic("error with inflate init");
     }
 
     inflate(&d_stream, Z_FINISH);
     if (err != Z_OK && err != Z_STREAM_END) {
-        panic("error with inflate\n");
+        panic("error with inflate");
     }
 
     err = inflateEnd(&d_stream);
     if (err != Z_OK) {
-        panic("error with inflate after ending\n");
+        panic("error with inflate after ending");
     }
 
     return d_stream.total_out;
 }
 
-uint convert_uint(u_char *buff) {
-    uint n = 0;
+uint convert_uint(uint8_t *buff) {
+    uint32_t n = 0;
     memcpy(&n, buff, 4);
     return __builtin_bswap32(n); // big endian to little endian
 }
@@ -155,129 +163,20 @@ bool validate_signature(FILE *file) {
 
     uint8_t buff[8];
 
-    if (!fread(buff, CHAR, 8, file)) panic("Couldn't read signature\n");
+    if (!fread(buff, CHAR, 8, file))
+        panic("Couldn't read signature");
 
     uint64_t orig;
     memcpy(&orig, buff, 8);
     orig = __builtin_bswap64(orig); // big endian to little endian
 
-    if (sig != orig) return false;
+    if (sig != orig)
+        return false;
 
     return true;
 }
 
-int main() {
-    FILE *file = fopen("pngs/chart.png", "rb");
-    if (file == NULL) {
-        perror("fopen");
-        exit(69);
-    }
-
-    if (!validate_signature(file))
-        panic("Invalid PNG signature\n");
-
-    size_t data_cap = sizeof(u_char) * 2048;
-    u_char *data = malloc(data_cap); // contains entire IDAT data
-    size_t data_t = 0;
-
-    uint width = 0;
-    uint height = 0;
-    uint bit_depth = 0;
-    uint color_type = 0;
-
-    char type[5];           // chunk type
-    u_char buff[128] = {1}; // reading file bytes into
-    while (1) {
-        uint length = 0;
-        if (fread(buff, CHAR, LENGTH, file) < 1)
-            break; // terminate if EOF
-        length = convert_uint(buff);
-
-        fread(buff, CHAR, CHAR * 4, file);
-        memmove(type, buff, 4);
-        type[4] = '\0';
-
-        if (strcmp(type, "IHDR") == 0) {
-            fread(buff, CHAR, LENGTH, file);
-            width = convert_uint(buff);
-
-            fread(buff, CHAR, LENGTH, file);
-            height = convert_uint(buff);
-
-            fread(buff, CHAR, CHAR, file);
-            bit_depth = buff[0];
-
-            fread(buff, CHAR, CHAR, file);
-            color_type = buff[0];
-
-            // skipping compression method, filter method and interlace because
-            // they are always the same
-            fseek(file, 3, SEEK_CUR);
-
-        } else if (strcmp(type, "IDAT") == 0) {
-            u_char chunk[length];
-            fread(chunk, CHAR, length, file);
-
-            if (data_cap - data_t < length) {
-                data_cap = (data_cap + length);
-                data = realloc(data, data_cap);
-            }
-
-            memmove(data + data_t, chunk, length);
-            data_t += length;
-        } else if (strcmp(type, "IEND") == 0) {
-            // everything already done
-        } else if (strcmp(type, "PLTE") == 0) {
-            panic("PLTE not handled\n");
-        } else {
-            printf("Auxillary chunk(%s) or some error!: %u\n", type, length);
-            fseek(file, length,
-                  SEEK_CUR); // FIXME: right now just skipping auxillary chunks
-        }
-
-        fseek(file, CRC, SEEK_CUR); // FIXME: skip CRC bytes
-    }
-
-    printf("image: %ux%u, %u depth and %u color type with %zu bytes data\n",
-           width, height, bit_depth, color_type, data_t);
-
-    int bpp = (color_type == 6) ? 4 : 3; // byte per pixel
-
-    // uncompress IDAT chunks
-    uLong uncomprLen = (width * bpp + 1) * height; // + 1 for filter byte
-    u_char *uncompressed = malloc(uncomprLen);
-    int bytes = zinflate(data, uncompressed, data_t, uncomprLen);
-
-    // apply filtering to uncompressed
-    uint8_t *idat = malloc(width * height * bpp);
-    recon(uncompressed, idat, width, height, bpp);
-
-    fclose(file);
-    free(uncompressed);
-    free(data);
-
-    u_char *raw = idat;
-    Image image = GenImageColor(width, height, BLACK); // write to image
-
-    for (int j = 0; j < height; j++) {
-        for (int i = 0; i < width; i++) {
-            if (!raw)
-                panic("raw is NULL\n");
-            Color color;
-            // FIXME: only supporting color_type 6 and 4
-            if (color_type == 6) {
-                color = (Color){raw[0], raw[1], raw[2], raw[3]};
-                raw += 4; // one pixel
-            } else {
-                color = (Color){raw[0], raw[1], raw[2], 255};
-                raw += 3; // one pixel
-            }
-            ImageDrawPixel(&image, i, j, color);
-        }
-    }
-
-    free(idat);
-
+void render(uint width, uint height, Image image) {
     // Raylib shit
     SetTraceLogLevel(LOG_ERROR);
     InitWindow(width, height, "Poder");
@@ -315,4 +214,120 @@ int main() {
     UnloadTexture(texture);
 
     CloseWindow();
+}
+
+int main() {
+    const char* pngfile = "pngs/chart.png";
+    FILE *file = fopen(pngfile, "rb");
+    if (file == NULL) {
+        perror("fopen");
+        exit(69);
+    }
+
+    if (!validate_signature(file))
+        panic("Invalid PNG signature");
+
+    size_t data_cap = sizeof(uint8_t) * 2048;
+    size_t data_t = 0;
+    uint8_t *data = malloc(data_cap); // contains entire compressed IDAT data
+
+    uint32_t width = 0;
+    uint32_t height = 0;
+    uint32_t bit_depth = 0;
+    uint32_t color_type = 0;
+
+    char type[5];            // chunk type
+    uint8_t buff[128] = {0}; // reading file bytes into
+    while (true) {
+        uint32_t length = 0;
+        if (fread(buff, CHAR, LENGTH, file) < 1)
+            break; // terminate if EOF
+        length = convert_uint(buff);
+
+        fread(buff, CHAR, CHAR * 4, file);
+        memmove(type, buff, 4);
+        type[4] = '\0';
+
+        if (strcmp(type, "IHDR") == 0) {
+            fread(buff, CHAR, LENGTH, file);
+            width = convert_uint(buff);
+
+            fread(buff, CHAR, LENGTH, file);
+            height = convert_uint(buff);
+
+            fread(buff, CHAR, CHAR, file);
+            bit_depth = buff[0];
+
+            fread(buff, CHAR, CHAR, file);
+            color_type = buff[0];
+
+            // skipping compression method, filter method and interlace because
+            // they are always the same
+            fseek(file, 3, SEEK_CUR);
+        } else if (strcmp(type, "IDAT") == 0) {
+            uint8_t chunk[length];
+            fread(chunk, CHAR, length, file);
+
+            if (data_cap - data_t < length) {
+                data_cap = (data_cap + length);
+                data = realloc(data, data_cap);
+            }
+
+            memmove(data + data_t, chunk, length);
+            data_t += length;
+        } else if (strcmp(type, "IEND") == 0) {
+            // everything already done
+        } else if (strcmp(type, "PLTE") == 0) {
+            panic("PLTE not handled");
+        } else {
+            printf("Auxillary chunk(%s) or some error!: %u\n", type, length);
+            fseek(file, length,
+                  SEEK_CUR); // FIXME: right now just skipping auxillary chunks
+        }
+
+        fseek(file, CRC, SEEK_CUR); // FIXME: skip CRC bytes
+    }
+
+    printf("%s: %ux%u, %u depth and %u color type with %zu bytes data\n",
+           pngfile, width, height, bit_depth, color_type, data_t);
+
+    uint32_t bpp = (color_type == 6) ? 4 : 3; // byte per pixel
+
+    // uncompress IDAT chunks
+    uLong uncomprLen = (width * bpp + 1) * height; // + 1 for filter byte
+    uint8_t *uncompressed = malloc(uncomprLen);
+    uLong bytes = zinflate(data, uncompressed, data_t, uncomprLen);
+
+    // apply filtering to uncompressed
+    uint8_t *idat = malloc(width * height * bpp);
+    recon(uncompressed, idat, width, height, bpp);
+
+    uint8_t *raw = idat;
+    Image image = GenImageColor(width, height, BLACK); // write to image
+
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            if (!raw)
+                panic("raw is NULL");
+            Color color;
+            // FIXME: only supporting color_type 6 and 4
+            if (color_type == COLOR_TRUEALPHA_RGBA) {
+                color = (Color){raw[0], raw[1], raw[2], raw[3]};
+                raw += 4; // one pixel
+            } else if (color_type == COLOR_TRUE_RGB) {
+                color = (Color){raw[0], raw[1], raw[2], 255};
+                raw += 3; // one pixel
+            } else {
+                panic("Poder does not support color type");
+            }
+            ImageDrawPixel(&image, i, j, color);
+        }
+    }
+
+    fclose(file);
+    free(uncompressed);
+    free(data);
+    free(idat);
+
+    render(width, height, image);
 }
